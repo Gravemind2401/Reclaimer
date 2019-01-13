1ef617a621cc678a7947a8252d6feeb05a951b15
Gravemind2401 <inferi_sententia@hotmail.com>
2019-01-13 18:53:44 +1100

    move DDS code to external project

--- Adjutant/Properties/AssemblyInfo.cs
+++ Adjutant/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.0.93")]
-[assembly: AssemblyFileVersion("5.0.0.93")]
+[assembly: AssemblyVersion("5.0.24.120")]
+[assembly: AssemblyFileVersion("5.0.24.120")]
