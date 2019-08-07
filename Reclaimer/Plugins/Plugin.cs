a3d42810a0e407e16e474f0df482ea55c04ed276
Gravemind2401 <inferi_sententia@hotmail.com>
2019-08-07 22:45:57 +1000

    add a bunch of xml documentation

--- Reclaimer/Plugins/Plugin.cs
+++ Reclaimer/Plugins/Plugin.cs
@@ -10,15 +10,30 @@ namespace Reclaimer.Plugins
     public delegate void MenuItemClickHandler(string key);
     public delegate void ContextItemClickHandler(string key, OpenFileArgs context);
 
+    /// <summary>
+    /// Represents a menu item that will be added to the application's menu bar.
+    /// </summary>
     public class PluginMenuItem
     {
         private readonly string key;
         private readonly MenuItemClickHandler handler;
 
+        /// <summary>
+        /// Gets the menu path to the menu item.
+        /// </summary>
         public string Path { get; }
 
+        /// <summary>
+        /// Executes the handler associated with the menu item.
+        /// </summary>
         public void ExecuteHandler() => handler(key);
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="PluginMenuItem"/> class.
+        /// </summary>
+        /// <param name="key">The unique key associated with this menu item.</param>
+        /// <param name="path">The menu path to this menu item.</param>
+        /// <param name="handler">The method to execute when the menu item is clicked.</param>
         public PluginMenuItem(string key, string path, MenuItemClickHandler handler)
         {
             if (key == null)
@@ -36,15 +51,31 @@ namespace Reclaimer.Plugins
         }
     }
 
+    /// <summary>
+    /// Represents a context menu item that will be displayed for a file object.
+    /// </summary>
     public class PluginContextItem
     {
         private readonly string key;
         private readonly ContextItemClickHandler handler;
 
+        /// <summary>
+        /// Gets the menu path to the context menu item.
+        /// </summary>
         public string Path { get; }
 
+        /// <summary>
+        /// Executes the handler associated with the context menu item.
+        /// </summary>
+        /// <param name="context">The file object that the context menu is attached to.</param>
         public void ExecuteHandler(OpenFileArgs context) => handler(key, context);
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="PluginContextItem"/> class.
+        /// </summary>
+        /// <param name="key">The unique key associated with this menu item.</param>
+        /// <param name="path">The menu path to this menu item.</param>
+        /// <param name="handler">The method to execute when the context menu item is clicked.</param>
         public PluginContextItem(string key, string path, ContextItemClickHandler handler)
         {
             if (key == null)
@@ -66,41 +97,97 @@ namespace Reclaimer.Plugins
     {
         internal readonly List<LogEntry> logEntries = new List<LogEntry>();
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="Plugin"/> class.
+        /// </summary>
         public Plugin() { }
 
         internal string Assembly => GetType().Assembly.FullName;
 
         internal virtual string Key => GetType().FullName;
 
+        /// <summary>
+        /// The name of the plugin.
+        /// </summary>
         public abstract string Name { get; }
 
+        /// <summary>
+        /// When overidden in a derived class, performs any initialisation required by the plugin.
+        /// This method is called by the <see cref="Substrate"/> when the plugin is first loaded.
+        /// </summary>
         public virtual void Initialise() { }
 
+        /// <summary>
+        /// When overidden in a derived class, performs any cleanup required by the plugin.
+        /// This method is called by the <see cref="Substrate"/> when the plugin is unloaded.
+        /// </summary>
         public virtual void Suspend() { }
 
+        /// <summary>
+        /// Reads the current plugin's settings from the application's settings file.
+        /// If the settings failed to be parsed the default value will be returned.
+        /// </summary>
+        /// <typeparam name="T">The type used to store the settings.</typeparam>
         protected T LoadSettings<T>() where T : new()
         {
             return Substrate.GetPluginSettings<T>(Key);
         }
 
+        /// <summary>
+        /// Saves the current plugin's settings to the application's settings file.
+        /// </summary>
+        /// <typeparam name="T">The type used to store the settings.</typeparam>
+        /// <param name="settings">The settings.</param>
         protected void SaveSettings<T>(T settings) where T : new()
         {
             Substrate.SavePluginSettings(Key, settings);
         }
 
+        /// <summary>
+        /// When overidden in a derived class, gets a value indicating if the current
+        /// plugin accepts a particular type of file object. If this method returns true,
+        /// the <see cref="Substrate"/> may proceed to call <see cref="OpenFile(OpenFileArgs)"/>.
+        /// </summary>
+        /// <param name="args">The file arguments.</param>
         public virtual bool CanOpenFile(OpenFileArgs args) => false;
 
+        /// <summary>
+        /// When overidden in a derived class, processes the request to handle a particular file object.
+        /// This method is called by the <see cref="Substrate"/> when the user requests for a file to be handled.
+        /// </summary>
+        /// <param name="args">The file arguments.</param>
         public virtual void OpenFile(OpenFileArgs args) { }
 
+        /// <summary>
+        /// When overidden in a derived class, gets a value indicating if the current
+        /// plugin can handle physical files of a particular file extension. If this method
+        /// returns true, the <see cref="Substrate"/> may proceed to call <see cref="OpenPhysicalFile(string fileName)"/>.
+        /// </summary>
+        /// <param name="extension">The file extension to handle.</param>
         public virtual bool SupportsFileExtension(string extension) => false;
 
+        /// <summary>
+        /// When overidden in a derived class, processes the request to handle a particular physical file.
+        /// This method is called by the <see cref="Substrate"/> when the user requests for a physical file to be opened.
+        /// </summary>
+        /// <param name="args">The file arguments.</param>
         public virtual void OpenPhysicalFile(string fileName) { }
 
+        /// <summary>
+        /// When overidden in a derived class, gets a list of menu items provided by the current plugin.
+        /// Any menu items returned by this method will be added to the application's menu bar.
+        /// </summary>
         public virtual IEnumerable<PluginMenuItem> GetMenuItems()
         {
             yield break;
         }
 
+        /// <summary>
+        /// When overidden in a derived class, gets a list of context menu items provided by the current plugin
+        /// for a particular file object. This method is called by the <see cref="Substrate"/> when another plugin
+        /// requests context items for a particular file object.
+        /// </summary>
+        /// <param name="context">The file arguments.</param>
         public virtual IEnumerable<PluginContextItem> GetContextItems(OpenFileArgs context)
         {
             yield break;
@@ -108,6 +195,10 @@ namespace Reclaimer.Plugins
 
         //themes (including existing)
 
+        /// <summary>
+        /// Logs a message to the output pane.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         protected internal void LogOutput(string message)
         {
             var entry = new LogEntry(DateTime.Now, message);
@@ -115,6 +206,11 @@ namespace Reclaimer.Plugins
             Substrate.LogOutput(this, entry);
         }
 
+        /// <summary>
+        /// Logs an error to the output pane with a related message.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="e">The error to log.</param>
         protected internal void LogError(string message, Exception e)
         {
             var entry = new LogEntry(DateTime.Now, $"{message}{Environment.NewLine}{e.ToString()}");
@@ -122,6 +218,9 @@ namespace Reclaimer.Plugins
             Substrate.LogOutput(this, entry);
         }
 
+        /// <summary>
+        /// Clears the current plugin's log message from the output pane.
+        /// </summary>
         protected internal void ClearLog()
         {
             logEntries.Clear();
--- Reclaimer/Plugins/Substrate.cs
+++ Reclaimer/Plugins/Substrate.cs
@@ -162,8 +162,15 @@ namespace Reclaimer.Plugins
         #endregion
 
         #region Public
+        /// <summary>
+        /// Gets the directory path that plugin assemblies are loaded from.
+        /// </summary>
         public static string PluginsDirectory => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Plugins");
 
+        /// <summary>
+        /// Opens a file object with the associated default handler.
+        /// </summary>
+        /// <param name="args">The file arguments.</param>
         public static bool OpenWithDefault(OpenFileArgs args)
         {
             var defaultHandler = GetDefaultHandler(args);
@@ -174,6 +181,10 @@ namespace Reclaimer.Plugins
             return true;
         }
 
+        /// <summary>
+        /// Prompts the user to select which handler to open a file object with.
+        /// </summary>
+        /// <param name="args">The file arguments.</param>
         public static bool OpenWithPrompt(OpenFileArgs args)
         {
             var defaultHandler = GetDefaultHandler(args);
@@ -187,11 +198,23 @@ namespace Reclaimer.Plugins
             return true;
         }
 
+        /// <summary>
+        /// Fetches context items from across all plugins for a particular file object.
+        /// </summary>
+        /// <param name="context">The file arguments.</param>
         public static IEnumerable<PluginContextItem> GetContextItems(OpenFileArgs context)
         {
             return AllPlugins.SelectMany(p => p.GetContextItems(context));
         }
 
+        /// <summary>
+        /// Adds a utility to the specified window. If a tab control exists in the target location
+        /// the utility will be added to the existing tab control, otherwise a new tab control will be created.
+        /// The size of the tab control will be set to the host's default dock size.
+        /// </summary>
+        /// <param name="item">The utility item to add.</param>
+        /// <param name="host">The window the utility will be added to.</param>
+        /// <param name="targetDock">The dock are the utility will be added to.</param>
         public static void AddUtility(ITabContent item, IMultiPanelHost host, Dock targetDock)
         {
             if (host == null)
@@ -200,6 +223,14 @@ namespace Reclaimer.Plugins
             AddUtility(item, host, targetDock, host.MultiPanel.DefaultDockSize);
         }
 
+        /// <summary>
+        /// Adds a utility to the specified window. If a tab control exists in the target location
+        /// the utility will be added to the existing tab control, otherwise a new tab control will be created.
+        /// The size of the tab control will be set to <param name="targetSize"/>.
+        /// </summary>
+        /// <param name="item">The utility item to add.</param>
+        /// <param name="host">The window the utility will be added to.</param>
+        /// <param name="targetDock">The dock are the utility will be added to.</param>
         public static void AddUtility(ITabContent item, IMultiPanelHost host, Dock targetDock, GridLength targetSize)
         {
             if (item == null)
@@ -247,6 +278,9 @@ namespace Reclaimer.Plugins
             tc.Items.Add(item);
         }
 
+        /// <summary>
+        /// Displays the output utility if it is not already visible.
+        /// </summary>
         public static void ShowOutput()
         {
             if (Controls.OutputViewer.Instance.Parent != null)
@@ -255,8 +289,15 @@ namespace Reclaimer.Plugins
             AddUtility(Controls.OutputViewer.Instance, GetHostWindow(), Dock.Bottom, new GridLength(250));
         }
 
+        /// <summary>
+        /// Gets the application's main window.
+        /// </summary>
         public static IMultiPanelHost GetHostWindow() => GetHostWindow(null);
 
+        /// <summary>
+        /// Gets the owner window of a particular <see cref="UIElement"/>.
+        /// </summary>
+        /// <param name="element">The element to find the host for.</param>
         public static IMultiPanelHost GetHostWindow(UIElement element)
         {
             IMultiPanelHost host;
@@ -268,11 +309,29 @@ namespace Reclaimer.Plugins
         #endregion
     }
 
+    /// <summary>
+    /// Contains details about a file object that may be displayed to the user or manipulated by plugins.
+    /// </summary>
     public class OpenFileArgs
     {
+        /// <summary>
+        /// The name of the file object.
+        /// </summary>
         public string FileName { get; }
+
+        /// <summary>
+        /// The file object.
+        /// </summary>
         public object File { get; }
+
+        /// <summary>
+        /// A unique key representing the type of file.
+        /// </summary>
         public string FileTypeKey { get; }
+
+        /// <summary>
+        /// The window the file should be sent to.
+        /// </summary>
         public IMultiPanelHost TargetWindow { get; }
 
         public OpenFileArgs(string fileName, object file, string key)
