699d682694f4da69a465f607b7f58bfecce3b373
Gravemind2401 <inferi_sententia@hotmail.com>
2019-08-11 15:57:53 +1000

    dont show OpenWithDialog in taskbar

--- Reclaimer/Properties/AssemblyInfo.cs
+++ Reclaimer/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.86.994")]
-[assembly: AssemblyFileVersion("1.0.86.994")]
+[assembly: AssemblyVersion("1.0.87.995")]
+[assembly: AssemblyFileVersion("1.0.87.995")]
