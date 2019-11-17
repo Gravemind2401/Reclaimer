88041bacf84147e284f72f09e522ba49f65b9fca
Gravemind2401 <inferi_sententia@hotmail.com>
2019-11-17 11:13:24 +1100

    initial code for PakViewer plugin

---
+++ Reclaimer/Controls/PakViewer.xaml.cs
@@ -0,0 +1,191 @@
+﻿using Adjutant.Saber3D.Common;
+using Reclaimer.Models;
+using Reclaimer.Plugins;
+using Reclaimer.Utilities;
+using Reclaimer.Windows;
+using Studio.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace Reclaimer.Controls
+{
+    /// <summary>
+    /// Interaction logic for MapViewer.xaml
+    /// </summary>
+    public partial class PakViewer : UserControl
+    {
+        private readonly MenuItem OpenContextItem;
+        private readonly MenuItem OpenWithContextItem;
+        private readonly Separator ContextSeparator;
+
+        private IPakFile pak;
+
+        public TabModel TabModel { get; }
+        public ObservableCollection<UIElement> ContextItems { get; }
+
+        public PakViewer()
+        {
+            InitializeComponent();
+
+            OpenContextItem = new MenuItem { Header = "Open" };
+            OpenWithContextItem = new MenuItem { Header = "Open With..." };
+            ContextSeparator = new Separator();
+
+            TabModel = new TabModel(this, TabItemType.Tool);
+            ContextItems = new ObservableCollection<UIElement>();
+
+            DataContext = this;
+        }
+
+        public void LoadPak(string fileName)
+        {
+            pak = new Adjutant.Saber3D.Halo1X.PakFile(fileName);
+
+            TabModel.Header = Utils.GetFileName(pak.FileName);
+            TabModel.ToolTip = $"Pak Viewer - {TabModel.Header}";
+
+            BuildItemTree(null);
+        }
+
+        private void BuildItemTree(string filter)
+        {
+            var result = new List<TreeNode>();
+            var itemGroups = pak.Items
+                .Where(i => FilterTag(filter, i))
+                .GroupBy(i => i.ItemType.ToString());
+
+            foreach (var g in itemGroups.OrderBy(g => g.Key))
+            {
+                var node = new TreeNode { Header = g.Key };
+                foreach (var item in g.OrderBy(i => i.Name))
+                {
+                    node.Children.Add(new TreeNode
+                    {
+                        Header = item.Name,
+                        Tag = item
+                    });
+                }
+                result.Add(node);
+            }
+
+            tv.ItemsSource = result;
+        }
+
+        private bool FilterTag(string filter, IPakItem item)
+        {
+            if (string.IsNullOrEmpty(filter))
+                return true;
+
+            if (item.Name.ToUpper() == filter.ToUpper())
+                return true;
+
+            return false;
+        }
+
+        private void RecursiveCollapseNode(TreeNode node)
+        {
+            foreach (var n in node.Children)
+                RecursiveCollapseNode(n);
+            node.IsExpanded = false;
+        }
+
+        private OpenFileArgs GetSelectedArgs()
+        {
+            var node = tv.SelectedItem as TreeNode;
+            if (node.HasChildren) //folder
+                return new OpenFileArgs(node.Header, $"Saber3D.Halo1X.*", node);
+
+            var item = node.Tag as IPakItem;
+            var fileName = $"{item.Name}.{item.ItemType}";
+            var fileKey = $"Saber3D.Halo1X.{item.ItemType}";
+            return new OpenFileArgs(fileName, fileKey, Substrate.GetHostWindow(this), item);
+        }
+
+        #region Event Handlers
+        private void btnCollapseAll_Click(object sender, RoutedEventArgs e)
+        {
+            var nodes = tv.ItemsSource as List<TreeNode>;
+
+            foreach (var node in nodes)
+                RecursiveCollapseNode(node);
+        }
+
+        private void txtSearch_SearchChanged(object sender, RoutedEventArgs e)
+        {
+            BuildItemTree(txtSearch.Text);
+        }
+
+        private void TreeItemPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            (sender as TreeViewItem).IsSelected = true;
+        }
+
+        private void TreeItemMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left)
+                return;
+
+            if ((sender as TreeViewItem)?.DataContext != tv.SelectedItem)
+                return; //because this event bubbles to the parent node
+
+            var item = (tv.SelectedItem as TreeNode)?.Tag as IPakItem;
+            if (item == null) return;
+
+            Substrate.OpenWithDefault(GetSelectedArgs());
+        }
+
+        private void TreeItemContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            foreach (MenuItem item in ContextItems.Where(i => i is MenuItem))
+                item.Click -= ContextItem_Click;
+
+            var menu = (sender as ContextMenu);
+            var node = tv.SelectedItem as TreeNode;
+
+            ContextItems.Clear();
+            if (node.Tag is IPakItem)
+            {
+                ContextItems.Add(OpenContextItem);
+                ContextItems.Add(OpenWithContextItem);
+            }
+
+            var customItems = Substrate.GetContextItems(GetSelectedArgs());
+
+            if (ContextItems.Any() && customItems.Any())
+                ContextItems.Add(ContextSeparator);
+
+            foreach (var item in customItems)
+                ContextItems.Add(new MenuItem { Header = item.Path, Tag = item });
+
+            foreach (MenuItem item in ContextItems.Where(i => i is MenuItem))
+                item.Click += ContextItem_Click;
+
+            if (!ContextItems.Any())
+                e.Handled = true;
+        }
+
+        private void ContextItem_Click(object sender, RoutedEventArgs e)
+        {
+            var args = GetSelectedArgs();
+            if (sender == OpenContextItem)
+                Substrate.OpenWithDefault(args);
+            else if (sender == OpenWithContextItem)
+                Substrate.OpenWithPrompt(args);
+            else ((sender as MenuItem)?.Tag as PluginContextItem)?.ExecuteHandler(args);
+        }
+        #endregion
+    }
+}
---
+++ Reclaimer/Plugins/PakViewerPlugin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Reclaimer.Plugins
+{
+    internal class PakViewerPlugin : Plugin
+    {
+        const string OpenKey = "PakViewer.OpenPak";
+        const string OpenPath = "File\\Open Pak";
+
+        internal static PakViewerSettings Settings;
+
+        public override string Name => "Pak Viewer";
+
+        public override void Initialise()
+        {
+            Settings = LoadSettings<PakViewerSettings>();
+        }
+
+        public override void Suspend()
+        {
+            SaveSettings(Settings);
+        }
+
+        public override IEnumerable<PluginMenuItem> GetMenuItems()
+        {
+            yield return new PluginMenuItem(OpenKey, OpenPath, OnMenuItemClick);
+        }
+
+        private void OnMenuItemClick(string key)
+        {
+            if (key != OpenKey) return;
+
+            var ofd = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Saber Pak Files|*.s3dpak",
+                Multiselect = true,
+                CheckFileExists = true
+            };
+
+            if (!string.IsNullOrEmpty(Settings.PakFolder))
+                ofd.InitialDirectory = Settings.PakFolder;
+
+            if (ofd.ShowDialog() != true)
+                return;
+
+            foreach (var fileName in ofd.FileNames)
+                OpenPhysicalFile(fileName);
+        }
+
+        public override bool SupportsFileExtension(string extension)
+        {
+            return extension.ToLower() == "s3dpak";
+        }
+
+        public override void OpenPhysicalFile(string fileName)
+        {
+            LogOutput($"Loading pak file: {fileName}");
+
+            var pv = new Controls.PakViewer();
+            pv.LoadPak(fileName);
+            Substrate.AddTool(pv.TabModel, Substrate.GetHostWindow(), Dock.Left, new GridLength(400));
+
+            LogOutput($"Loaded pak file: {fileName}");
+        }
+    }
+
+    internal class PakViewerSettings
+    {
+        public string PakFolder { get; set; }
+    }
+}
