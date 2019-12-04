d304b04146c3c741e099fe7285533a8be943e3fc
Gravemind2401 <inferi_sententia@hotmail.com>
2019-12-04 21:43:50 +1100

    update output paths on MetaViewer plugin files

--- Reclaimer/Properties/AssemblyInfo.cs
+++ Reclaimer/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.107.1121")]
-[assembly: AssemblyFileVersion("1.0.107.1121")]
+[assembly: AssemblyVersion("1.0.108.1123")]
+[assembly: AssemblyFileVersion("1.0.108.1123")]
