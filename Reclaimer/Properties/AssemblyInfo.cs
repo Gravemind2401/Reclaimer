7a96faef5a7de284c0de7dd3993b8014f0d3eec1
Gravemind2401 <inferi_sententia@hotmail.com>
2020-07-08 21:56:09 +1000

    1.4.175 release

--- Reclaimer/Properties/AssemblyInfo.cs
+++ Reclaimer/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.174.1394")]
-[assembly: AssemblyFileVersion("1.4.174.1394")]
+[assembly: AssemblyVersion("1.4.175.1394")]
+[assembly: AssemblyFileVersion("1.4.175.1394")]
