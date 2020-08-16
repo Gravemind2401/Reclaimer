bdfbb65e7cf474bca091584b752fc866ceef859a
Gravemind2401 <inferi_sententia@hotmail.com>
2020-08-16 20:42:16 +1000

    add TagBlock for manual block reading

---
+++ Adjutant/Blam/Common/TagBlock.cs
@@ -0,0 +1,36 @@
+﻿using Adjutant.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.Endian;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adjutant.Blam.Common
+{
+    [FixedSize(12, MaxVersion = (int)CacheType.Halo2Xbox)]
+    [FixedSize(8, MinVersion = (int)CacheType.Halo2Xbox, MaxVersion = (int)CacheType.Halo3Beta)]
+    [FixedSize(12, MinVersion = (int)CacheType.Halo3Beta)]
+    public class TagBlock
+    {
+        public int Count { get; }
+        public Pointer Pointer { get; }
+
+        public TagBlock(DependencyReader reader, ICacheFile cache, IAddressTranslator translator)
+            : this(reader, cache, translator, null)
+        { }
+        
+        public TagBlock(DependencyReader reader, ICacheFile cache, IAddressTranslator translator, IPointerExpander expander)
+        {
+            if (reader == null)
+                throw new ArgumentNullException(nameof(reader));
+
+            if (translator == null)
+                throw new ArgumentNullException(nameof(translator));
+
+            Count = reader.ReadInt32();
+            Pointer = new Pointer(reader.ReadInt32(), translator, expander);
+        }
+    }
+}
--- Adjutant/Properties/AssemblyInfo.cs
+++ Adjutant/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.4.215.1346")]
-[assembly: AssemblyFileVersion("5.4.215.1346")]
+[assembly: AssemblyVersion("5.4.216.1349")]
+[assembly: AssemblyFileVersion("5.4.216.1349")]
