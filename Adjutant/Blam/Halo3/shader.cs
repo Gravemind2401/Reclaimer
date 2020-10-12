8ba6c4b750500db8359b93c0f3fd9bab49977b2c
Gravemind2401 <inferi_sententia@hotmail.com>
2020-10-12 21:10:06 +1100

    fix mcc halo3 materials issue

--- Adjutant/Blam/Halo3/shader.cs
+++ Adjutant/Blam/Halo3/shader.cs
@@ -37,7 +37,7 @@ namespace Adjutant.Blam.Halo3
         [Offset(0)]
         public TagReference BitmapReference { get; set; }
 
-        [Offset(20)]
-        public short TilingIndex { get; set; }
+        [Offset(21)]
+        public byte TilingIndex { get; set; }
     }
 }
--- Adjutant/Properties/AssemblyInfo.cs
+++ Adjutant/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.5.227.1372")]
-[assembly: AssemblyFileVersion("5.5.227.1372")]
+[assembly: AssemblyVersion("5.5.228.1373")]
+[assembly: AssemblyFileVersion("5.5.228.1373")]
