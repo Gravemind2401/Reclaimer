5e044c5a97117f211af86f1e768a59378862d130
Gravemind2401 <inferi_sententia@hotmail.com>
2020-10-13 18:55:29 +1100

    rebuild with mcc h3 vertex buffer fix

--- Reclaimer/Properties/AssemblyInfo.cs
+++ Reclaimer/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.204.1456")]
-[assembly: AssemblyFileVersion("1.5.204.1456")]
+[assembly: AssemblyVersion("1.5.205.1456")]
+[assembly: AssemblyFileVersion("1.5.205.1456")]
