38e907bf03640f254cf7c7368c17e7e1d43d0b54
Gravemind2401 <inferi_sententia@hotmail.com>
2021-03-29 19:21:27 +1100

    fix some h3 bsps not reading

--- Adjutant/Blam/Halo3/scenario_structure_bsp.cs
+++ Adjutant/Blam/Halo3/scenario_structure_bsp.cs
@@ -102,8 +102,8 @@ namespace Adjutant.Blam.Halo3
 
             var lightmap = scenario.ScenarioLightmapReference.Tag.ReadMetadata<scenario_lightmap>();
             var lightmapData = cache.CacheType < CacheType.MccHalo3U4
-                ? lightmap.LightmapData[bspIndex]
-                : lightmap.LightmapRefs[bspIndex].Tag.ReadMetadata<scenario_lightmap_bsp_data>();
+                ? lightmap.LightmapData.First(sldt => sldt.BspIndex == bspIndex)
+                : lightmap.LightmapRefs.Select(sldt => sldt.Tag.ReadMetadata<scenario_lightmap_bsp_data>()).First(sldt => sldt.BspIndex == bspIndex);
 
             model.Bounds.AddRange(BoundingBoxes);
             model.Materials.AddRange(Halo3Common.GetMaterials(Shaders));
