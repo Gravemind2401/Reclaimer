2da1f8ea509c1433b6d599a167e1998f38b78227
Gravemind2401 <inferi_sententia@hotmail.com>
2021-05-10 20:45:11 +1000

    add basic MenuButton control

---
+++ Reclaimer/Controls/MenuButton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Reclaimer.Controls
+{
+    [TemplatePart(Name = PART_Popup, Type = typeof(Popup))]
+    public class MenuButton : Button
+    {
+        private const string PART_Popup = "PART_Popup";
+
+        private Popup submenuPopup;
+
+        public static readonly DependencyProperty MenuItemsProperty =
+            DependencyProperty.Register(nameof(MenuItems), typeof(ObservableCollection<Control>), typeof(MenuButton), new PropertyMetadata(new ObservableCollection<Control>()));
+
+        public ObservableCollection<Control> MenuItems
+        {
+            get { return (ObservableCollection<Control>)GetValue(MenuItemsProperty); }
+            set { SetValue(MenuItemsProperty, value); }
+        }
+
+        static MenuButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MenuButton), new FrameworkPropertyMetadata(typeof(MenuButton)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            submenuPopup = GetTemplateChild(PART_Popup) as Popup;
+        }
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+
+            if (submenuPopup != null)
+                submenuPopup.IsOpen = true;
+        }
+    }
+}
---
+++ Reclaimer/Controls/MenuButtonItemsControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Reclaimer.Controls
+{
+    public class MenuButtonItemsControl : ItemsControl
+    {
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is MenuItem || item is Separator;
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new MenuItem();
+        }
+
+        protected override bool ShouldApplyItemContainerStyle(DependencyObject container, object item)
+        {
+            if (item is Separator)
+                return false;
+
+            return base.ShouldApplyItemContainerStyle(container, item);
+        }
+    }
+}
