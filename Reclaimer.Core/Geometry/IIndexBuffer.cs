7be4359eb3650f0920a42e4b17060075bec5263c
Gravemind2401 <inferi_sententia@hotmail.com>
2022-11-11 20:00:45 +1100

    add IndexBuffer

---
+++ Reclaimer.Core/Geometry/IIndexBuffer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reclaimer.Geometry
+{
+    public interface IIndexBuffer : IReadOnlyList<int>
+    {
+        void SwapEndianness();
+        int this[Index index] => this[index.GetOffset(Count)];
+        IEnumerable<int> this[Range range] => Extensions.Subset(this, range);
+    }
+}
---
+++ Reclaimer.Core/Geometry/IndexBuffer.cs
@@ -0,0 +1,81 @@
+﻿using Reclaimer.IO;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reclaimer.Geometry
+{
+    public class IndexBuffer : DataBuffer<int>, IIndexBuffer
+    {
+        private delegate int ReadMethod(ReadOnlySpan<byte> buffer);
+        private delegate void WriteMethod(int value, Span<byte> buffer);
+
+        public static IndexBuffer FromArray(byte[] data, Type dataType)
+        {
+            if (dataType == typeof(byte))
+                return new IndexBuffer(data, sizeof(byte));
+            else if (dataType == typeof(ushort))
+                return new IndexBuffer(data, sizeof(ushort));
+            else if (dataType == typeof(byte))
+                return new IndexBuffer(data, sizeof(int));
+            else
+                throw new ArgumentException("Data type must be byte, ushort or int.", nameof(dataType));
+        }
+
+        private readonly ReadMethod GetValue;
+        private readonly WriteMethod SetValue;
+
+        protected override int SizeOf { get; }
+
+        private IndexBuffer(byte[] buffer, int size)
+            : base(buffer, buffer.Length / size, 0, size, 0)
+        {
+            SizeOf = size;
+
+            if (size == sizeof(byte))
+            {
+                GetValue = data => data[0];
+                SetValue = (i, data) =>
+                {
+                    if (i > byte.MaxValue)
+                        throw new ArgumentOutOfRangeException();
+
+                    data[0] = (byte)i;
+                };
+            }
+            else if (size == sizeof(ushort))
+            {
+                GetValue = data => BitConverter.ToUInt16(data);
+                SetValue = (i, data) =>
+                {
+                    if (i > byte.MaxValue)
+                        throw new ArgumentOutOfRangeException();
+
+                    BitConverter.GetBytes((ushort)i).CopyTo(data);
+                };
+            }
+            else
+            {
+                GetValue = data => BitConverter.ToInt32(data);
+                SetValue = (i, data) => BitConverter.GetBytes(i).CopyTo(data);
+            }
+        }
+
+        public override int this[int index]
+        {
+            get => GetValue(CreateSpan(index));
+            set => SetValue(value, CreateSpan(index));
+        }
+
+        public void SwapEndianness()
+        {
+            if (SizeOf == 1)
+                return;
+
+            for (var i = 0; i < Count; i++)
+                CreateSpan(i).Reverse();
+        }
+    }
+}
