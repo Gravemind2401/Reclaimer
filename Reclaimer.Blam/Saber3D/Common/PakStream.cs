9ba13c8a8483909d7994a619e8d2e688dc3c9efa
Gravemind2401 <inferi_sententia@hotmail.com>
2023-01-08 19:41:01 +1100

    move most chunk logic to a core class to potentially be used with other kinds of files in future

--- Reclaimer.Blam/Saber3D/Common/PakStream.cs
+++ Reclaimer.Blam/Saber3D/Common/PakStream.cs
@@ -1,61 +1,31 @@
 ﻿using Reclaimer.IO;
-using System;
-using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 
 namespace Reclaimer.Saber3D.Common
 {
-    public class PakStream : Stream
+    public class PakStream : ChunkStream
     {
-        private record struct Chunk(int CompressedAddress, int CompressedSize, int UncompressedAddress, int UncompressedSize);
-
-        private readonly Stream baseStream;
-        private readonly Chunk[] chunks;
-
-        private bool positionDirty;
-        private ZLibStream chunkStream;
-
-        public override bool CanRead => true;
-        public override bool CanSeek => true;
-        public override bool CanWrite => false;
-
-        public override long Length { get; }
-
-        private long position;
-        public override long Position
-        {
-            get => position;
-            set => Seek(value, SeekOrigin.Begin);
-        }
-
         public PakStream(string filePath)
-            : this(new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            : base(filePath)
         { }
 
-        public PakStream(Stream baseStream)
+        protected override IList<(int, int, int)> ReadChunks()
         {
-            if (!baseStream.CanRead || !baseStream.CanSeek)
-                throw new NotSupportedException();
-
-            this.baseStream = baseStream;
-
-            using var reader = new EndianReader(baseStream, ByteOrder.LittleEndian, true);
+            using var reader = new EndianReader(BaseStream, ByteOrder.LittleEndian, true);
 
             reader.Seek(0, SeekOrigin.Begin);
 
             var chunkCount = reader.ReadInt32();
-            chunks = new Chunk[chunkCount];
+            var chunks = new (int, int, int)[chunkCount];
 
             var offsets = new int[chunkCount + 1];
-            offsets[^1] = (int)baseStream.Length;
+            offsets[^1] = (int)BaseStream.Length;
 
             for (var i = 0; i < chunkCount; i++)
                 offsets[i] = reader.ReadInt32();
 
-            var dataAddress = 0;
             for (var i = 0; i < chunkCount; i++)
             {
                 reader.Seek(offsets[i], SeekOrigin.Begin);
@@ -63,10 +33,9 @@ namespace Reclaimer.Saber3D.Common
                 var chunkAddress = (int)reader.Position;
                 var chunkSize = offsets[i + 1] - chunkAddress;
 
-                chunks[i] = new Chunk(chunkAddress, chunkSize, dataAddress, dataSize);
-                dataAddress += dataSize;
+                chunks[i] = (chunkAddress, chunkSize, dataSize);
             }
-            
+
             //attempt to correct for x360 files except they arent zlib?
             /*if (chunks.Any(c => c.UncompressedSize < 0 || c.UncompressedSize > 0x007FFFFF))
             {
@@ -91,100 +60,12 @@ namespace Reclaimer.Saber3D.Common
                 }
             }*/
 
-            Length = chunks.Sum(c => c.UncompressedSize);
+            return chunks;
         }
 
-        private void ChunkSeek(int bytesToSkip)
+        protected override Stream GetChunkStream(byte[] chunkData)
         {
-            if (bytesToSkip == 0)
-                return;
-
-            var buffer = new byte[0x40000];
-            do
-            {
-                bytesToSkip -= chunkStream.Read(buffer, 0, Math.Min(bytesToSkip, buffer.Length));
-            }
-            while (bytesToSkip > 0);
+            return new ZLibStream(new MemoryStream(chunkData), CompressionMode.Decompress);
         }
-
-        private void LoadChunk()
-        {
-            //TODO:if position has moved forward within the same chunk, just use ChunkSeek() instead of reloading the chunk
-
-            UnloadChunk();
-
-            var nextChunk = chunks.SkipWhile(c => c.UncompressedAddress > position).First();
-            baseStream.Seek(nextChunk.CompressedAddress, SeekOrigin.Begin);
-
-            var chunkData = new byte[nextChunk.CompressedSize];
-            baseStream.ReadAll(chunkData, 0, chunkData.Length);
-
-            var ms = new MemoryStream(chunkData);
-            chunkStream = new ZLibStream(ms, CompressionMode.Decompress);
-
-            //if position is part way through the chunk we need to consume the deflate stream until we reach the correct position
-            var seekBytes = (int)position - nextChunk.UncompressedAddress;
-            ChunkSeek(seekBytes);
-        }
-
-        private void UnloadChunk()
-        {
-            chunkStream?.Dispose();
-            chunkStream = null;
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            offset = origin switch
-            {
-                SeekOrigin.Current => position + offset,
-                SeekOrigin.End => Length + offset,
-                _ => offset
-            };
-
-            if (offset < 0 || offset >= Length)
-                throw new ArgumentOutOfRangeException(nameof(offset));
-
-            positionDirty = offset != position;
-            return position = offset;
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            if (buffer == null)
-                throw new ArgumentNullException(nameof(buffer));
-
-            if (offset < 0 || offset >= buffer.Length)
-                throw new ArgumentOutOfRangeException(nameof(offset));
-
-            if (offset + count > buffer.Length)
-                throw new ArgumentOutOfRangeException(nameof(count));
-
-            if (position >= Length)
-                return 0;
-
-            var bytesRemaining = count;
-
-            do
-            {
-                if (chunkStream == null || positionDirty)
-                    LoadChunk();
-
-                var bytesRead = chunkStream.Read(buffer, offset, bytesRemaining);
-                bytesRemaining -= bytesRead;
-                position += bytesRead;
-
-                //we reached the end of the chunk and need to load the next one
-                if (bytesRemaining > 0)
-                    positionDirty = true;
-            }
-            while (position < Length && bytesRemaining > 0);
-
-            return count - bytesRemaining;
-        }
-
-        public override void SetLength(long value) => throw new NotSupportedException();
-        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
-        public override void Flush() => throw new NotSupportedException();
     }
 }
---
+++ Reclaimer.Core/IO/ChunkStream.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Reclaimer.IO
+{
+    public abstract class ChunkStream : Stream
+    {
+        private readonly ChunkDescriptor[] chunks;
+        private readonly ChunkTracker chunkTracker;
+
+        //set initial value to true to ensure first read triggers a chunk update
+        private bool positionDirty = true;
+
+        protected Stream BaseStream { get; }
+        public override long Length { get; }
+
+        public override bool CanRead => true;
+        public override bool CanSeek => true;
+        public override bool CanWrite => false;
+
+        private long position;
+        public sealed override long Position
+        {
+            get => position;
+            set => Seek(value, SeekOrigin.Begin);
+        }
+
+        public ChunkStream(string filePath)
+            : this(new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        { }
+
+        public ChunkStream(Stream baseStream)
+        {
+            if (baseStream == null)
+                throw new ArgumentNullException(nameof(baseStream));
+
+            if (!baseStream.CanRead || !baseStream.CanSeek)
+                throw new NotSupportedException($"{nameof(baseStream)} must be readable and seekable");
+
+            BaseStream = baseStream;
+            chunkTracker = new ChunkTracker(this);
+
+            var chunkDetails = ReadChunks();
+            chunks = new ChunkDescriptor[chunkDetails.Count];
+
+            var destAddress = 0;
+            for (var i = 0; i < chunkDetails.Count; i++)
+            {
+                var (sourceAddress, compressedSize, uncompressedSize) = chunkDetails[i];
+                chunks[i] = new ChunkDescriptor(sourceAddress, compressedSize, destAddress, uncompressedSize);
+                destAddress += uncompressedSize;
+            }
+
+            Length = chunks.Sum(c => c.UncompressedSize);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            offset = origin switch
+            {
+                SeekOrigin.Current => position + offset,
+                SeekOrigin.End => Length + offset,
+                _ => offset
+            };
+
+            if (offset < 0 || offset >= Length)
+                throw new ArgumentOutOfRangeException(nameof(offset));
+
+            //if position was manually changed we need to refresh the current chunk on next read
+            positionDirty = offset != position;
+
+            return position = offset;
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (buffer == null)
+                throw new ArgumentNullException(nameof(buffer));
+
+            if (offset < 0 || offset >= buffer.Length)
+                throw new ArgumentOutOfRangeException(nameof(offset));
+
+            if (offset + count > buffer.Length)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            if (position < 0)
+                throw new ArgumentOutOfRangeException("Attempted to read before the beginning of the stream");
+
+            if (position >= Length)
+                return 0;
+
+            if (positionDirty)
+            {
+                //save time by using a dirty flag instead of looking up and comparing the current chunk every read
+                chunkTracker.PrepareChunk();
+                positionDirty = false;
+            }
+
+            var bytesRemaining = count;
+
+            do
+            {
+                var bytesRead = chunkTracker.ChunkStream.Read(buffer, offset, bytesRemaining);
+                bytesRemaining -= bytesRead;
+                position += bytesRead;
+
+                if (chunkTracker.IsEndOfChunk)
+                    chunkTracker.PrepareChunk();
+            }
+            while (position < Length && bytesRemaining > 0);
+
+            return count - bytesRemaining;
+        }
+
+        public override void SetLength(long value) => throw new NotSupportedException();
+        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+        public override void Flush() => throw new NotSupportedException();
+
+        protected abstract IList<(int SourceAddress, int CompressedSize, int UncompressedSize)> ReadChunks();
+        protected abstract Stream GetChunkStream(byte[] chunkData);
+
+        private record struct ChunkDescriptor(int SourceAddress, int CompressedSize, int DestAddress, int UncompressedSize)
+        {
+            public bool Contains(int address) => address >= DestAddress && address < DestAddress + UncompressedSize;
+        }
+
+        private sealed class ChunkTracker
+        {
+            private readonly ChunkStream sourceStream;
+
+            public ChunkDescriptor CurrentChunk { get; private set; }
+            public byte[] CompressedData { get; private set; }
+            public Stream ChunkStream { get; private set; }
+
+            public long InnerPosition => sourceStream.Position - CurrentChunk.DestAddress;
+            public bool IsEndOfChunk => !CurrentChunk.Contains((int)sourceStream.Position);
+
+            public ChunkTracker(ChunkStream sourceStream)
+            {
+                this.sourceStream = sourceStream;
+            }
+
+            public void PrepareChunk()
+            {
+                var nextChunk = sourceStream.chunks.First(c => c.Contains((int)sourceStream.Position));
+                if (nextChunk != CurrentChunk)
+                {
+                    CloseChunk();
+
+                    sourceStream.BaseStream.Seek(nextChunk.SourceAddress, SeekOrigin.Begin);
+
+                    CompressedData = new byte[nextChunk.UncompressedSize];
+                    sourceStream.BaseStream.ReadAll(CompressedData, 0, CompressedData.Length);
+
+                    CurrentChunk = nextChunk;
+                }
+
+                //always reload the chunk stream to make sure it starts at 0 again
+                ChunkStream = sourceStream.GetChunkStream(CompressedData);
+
+                if (InnerPosition == 0)
+                    return;
+                else if (ChunkStream.CanSeek)
+                {
+                    ChunkStream.Position = InnerPosition;
+                    return;
+                }
+
+                //the only way to move forward now is to read until we get to the desired position
+                var remaining = InnerPosition;
+                int bytesRead;
+                do
+                {
+                    var bufferSize = Math.Min((int)remaining, 0x10000);
+                    var buffer = MemoryPool<byte>.Shared.Rent(bufferSize);
+                    var span = buffer.Memory.Span[..bufferSize]; //in case we got more than we wanted
+
+                    remaining -= bytesRead = ChunkStream.ReadAll(span);
+                }
+                while (remaining > 0 && bytesRead > 0);
+            }
+
+            public void CloseChunk()
+            {
+                ChunkStream?.Close();
+                ChunkStream = null;
+                CompressedData = null;
+                CurrentChunk = default;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            chunkTracker.CloseChunk();
+            base.Dispose(disposing);
+        }
+    }
+}
