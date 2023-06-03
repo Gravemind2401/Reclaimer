babeb7a03e9abfe6d30ce4d86d2aa5f565e3a14d
Gravemind2401 <inferi_sententia@hotmail.com>
2023-06-03 18:53:04 +1000

    apply new settings and functions to max class

--- Reclaimer.Integration3d/reclaimer/autodesk/SceneBuilder.py
+++ Reclaimer.Integration3d/reclaimer/autodesk/SceneBuilder.py
@@ -1,5 +1,5 @@
 from typing import cast
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 from functools import reduce
 import operator
 
@@ -32,11 +32,24 @@ def create_scene(scene: Scene, options: ImportOptions = None):
     for model in scene.model_pool:
         builder = ModelBuilder(scene, model)
         if OPTIONS.IMPORT_BONES and model.bones:
+            print('creating skeleton')
             builder.create_bones()
-        # if OPTIONS.IMPORT_MARKERS and model.markers:
-        #     builder.create_markers()
         # if OPTIONS.IMPORT_MESHES and model.meshes:
+        #     print('creating meshes')
         #     builder.create_meshes()
+        # if OPTIONS.IMPORT_MARKERS and model.markers:
+        #     print('creating markers')
+        #     builder.create_markers()
+
+def _convert_transform_units(transform: Matrix4x4, bone_mode: bool = False) -> rt.Matrix3:
+    ''' Converts a transform from model units to max units '''
+    if not bone_mode:
+        return toMatrix3(transform) * rt.scaleMatrix(rt.Point3(UNIT_SCALE, UNIT_SCALE, UNIT_SCALE))
+
+    # for bones we want to keep the scale component at 1x, but still need to convert the translation component
+    m = toMatrix3(transform)
+    return rt.preRotate(rt.transMatrix(m.translationPart * UNIT_SCALE), m.rotationPart)
+
 
 class ModelBuilder:
     _scene: Scene
@@ -46,28 +59,27 @@ class ModelBuilder:
         self._scene = scene
         self._model = model
 
+    def _get_bone_transforms(self) -> List[rt.Matrix3]:
+        result = []
+        for bone in self._model.bones:
+            lineage = self._model.get_bone_lineage(bone)
+            transforms = [_convert_transform_units(x.transform, True) for x in reversed(lineage)]
+            result.append(reduce(operator.mul, transforms))
+        return result
+
     def create_bones(self):
         scene, model = self._scene, self._model
-        print('creating skeleton')
 
-        BONE_SIZE = 0.03 * UNIT_SCALE
+        BONE_SIZE = 0.03 * UNIT_SCALE * OPTIONS.BONE_SCALE
         TAIL_VECTOR = rt.Point3(BONE_SIZE, 0.0, 0.0)
 
-        def makemat(mat: Matrix4x4) -> rt.Matrix3:
-            m = toMatrix3(mat)
-            return rt.preRotate(rt.transMatrix(m.translationPart * UNIT_SCALE), m.rotationPart)
-        
-        bone_transforms: list[rt.Matrix3] = []
-        for b in model.bones:
-            lineage = model.get_bone_lineage(b)
-            lst = [makemat(x.transform) for x in reversed(lineage)]
-            bone_transforms.append(reduce(operator.mul, lst))
+        bone_transforms = self._get_bone_transforms()
 
         maxbones = []
         for i, b in enumerate(model.bones):
             maxbone = rt.BoneSys.createBone(rt.Point3(0, 0, 0), TAIL_VECTOR, rt.Point3(0, 0, 1))
             maxbone.setBoneEnable(False, 0)
-            maxbone.name = OPTIONS.BONE_PREFIX + b.name
+            maxbone.name = OPTIONS.bone_name(b)
             maxbone.height = maxbone.width = maxbone.length = BONE_SIZE
             maxbones.append(maxbone)
 
