8227c0af325cd788e33e1ee34156a515fbbd72e5
Gravemind2401 <inferi_sententia@hotmail.com>
2023-08-06 15:27:13 +1000

    add tests for struct read/write

---
+++ Reclaimer.Core.Tests/IO.Tests/DynamicRead/Struct.cs
@@ -0,0 +1,166 @@
+﻿namespace Reclaimer.IO.Tests.DynamicRead
+{
+    public partial class DynamicRead
+    {
+        [DataTestMethod]
+        [DataRow(ByteOrder.LittleEndian)]
+        [DataRow(ByteOrder.BigEndian)]
+        public void Struct01(ByteOrder order)
+        {
+            var rng = new Random();
+            using (var stream = new MemoryStream(new byte[500]))
+            using (var reader = new EndianReader(stream, order))
+            using (var writer = new EndianWriter(stream, order))
+            {
+                var rand = new object[12];
+
+                rand[0] = (sbyte)rng.Next(sbyte.MinValue, sbyte.MaxValue);
+                writer.Seek(0x00, SeekOrigin.Begin);
+                writer.Write((sbyte)rand[0]);
+
+                rand[1] = (short)rng.Next(short.MinValue, short.MaxValue);
+                writer.Seek(0x10, SeekOrigin.Begin);
+                writer.Write((short)rand[1]);
+
+                rand[2] = rng.Next(int.MinValue, int.MaxValue);
+                writer.Seek(0x20, SeekOrigin.Begin);
+                writer.Write((int)rand[2]);
+
+                rand[3] = (long)rng.Next(int.MinValue, int.MaxValue);
+                writer.Seek(0x30, SeekOrigin.Begin);
+                writer.Write((long)rand[3]);
+
+                rand[4] = (byte)rng.Next(byte.MinValue, byte.MaxValue);
+                writer.Seek(0x40, SeekOrigin.Begin);
+                writer.Write((byte)rand[4]);
+
+                rand[5] = (ushort)rng.Next(ushort.MinValue, ushort.MaxValue);
+                writer.Seek(0x50, SeekOrigin.Begin);
+                writer.Write((ushort)rand[5]);
+
+                rand[6] = unchecked((uint)rng.Next(int.MinValue, int.MaxValue));
+                writer.Seek(0x60, SeekOrigin.Begin);
+                writer.Write((uint)rand[6]);
+
+                rand[7] = (ulong)unchecked((uint)rng.Next(int.MinValue, int.MaxValue));
+                writer.Seek(0x70, SeekOrigin.Begin);
+                writer.Write((ulong)rand[7]);
+
+                rand[8] = (Half)rng.NextDouble();
+                writer.Seek(0x80, SeekOrigin.Begin);
+                writer.Write((Half)rand[8]);
+
+                rand[9] = (float)rng.NextDouble();
+                writer.Seek(0x90, SeekOrigin.Begin);
+                writer.Write((float)rand[9]);
+
+                rand[10] = rng.NextDouble();
+                writer.Seek(0xA0, SeekOrigin.Begin);
+                writer.Write((double)rand[10]);
+
+                rand[11] = Guid.NewGuid();
+                writer.Seek(0xB0, SeekOrigin.Begin);
+                writer.Write((Guid)rand[11]);
+
+                stream.Position = 0;
+                var obj = reader.ReadObject<BasicStruct01>();
+
+                Assert.AreEqual(0xFF, stream.Position);
+                Assert.AreEqual(rand[0], obj.Property1);
+                Assert.AreEqual(rand[1], obj.Property2);
+                Assert.AreEqual(rand[2], obj.Property3);
+                Assert.AreEqual(rand[3], obj.Property4);
+                Assert.AreEqual(rand[4], obj.Property5);
+                Assert.AreEqual(rand[5], obj.Property6);
+                Assert.AreEqual(rand[6], obj.Property7);
+                Assert.AreEqual(rand[7], obj.Property8);
+                Assert.AreEqual(rand[8], obj.Property9);
+                Assert.AreEqual(rand[9], obj.Property10);
+                Assert.AreEqual(rand[10], obj.Property11);
+                Assert.AreEqual(rand[11], obj.Property12);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(ByteOrder.LittleEndian)]
+        [DataRow(ByteOrder.BigEndian)]
+        public void Struct02(ByteOrder order)
+        {
+            var rng = new Random();
+            using (var stream = new MemoryStream(new byte[500]))
+            using (var reader = new EndianReader(stream, order))
+            using (var writer = new EndianWriter(stream, order))
+            {
+                var rand = new object[12];
+
+                rand[0] = (sbyte)rng.Next(sbyte.MinValue, sbyte.MaxValue);
+                writer.Seek(0x70, SeekOrigin.Begin);
+                writer.Write((sbyte)rand[0]);
+
+                rand[1] = (short)rng.Next(short.MinValue, short.MaxValue);
+                writer.Seek(0x40, SeekOrigin.Begin);
+                writer.Write((short)rand[1]);
+
+                rand[2] = rng.Next(int.MinValue, int.MaxValue);
+                writer.Seek(0x30, SeekOrigin.Begin);
+                writer.Write((int)rand[2]);
+
+                rand[3] = (long)rng.Next(int.MinValue, int.MaxValue);
+                writer.Seek(0x10, SeekOrigin.Begin);
+                writer.Write((long)rand[3]);
+
+                rand[4] = (byte)rng.Next(byte.MinValue, byte.MaxValue);
+                writer.Seek(0x90, SeekOrigin.Begin);
+                writer.Write((byte)rand[4]);
+
+                rand[5] = (ushort)rng.Next(ushort.MinValue, ushort.MaxValue);
+                writer.Seek(0xA0, SeekOrigin.Begin);
+                writer.Write((ushort)rand[5]);
+
+                rand[6] = unchecked((uint)rng.Next(int.MinValue, int.MaxValue));
+                writer.Seek(0x00, SeekOrigin.Begin);
+                writer.Write((uint)rand[6]);
+
+                rand[7] = (ulong)unchecked((uint)rng.Next(int.MinValue, int.MaxValue));
+                writer.Seek(0x80, SeekOrigin.Begin);
+                writer.Write((ulong)rand[7]);
+
+                rand[8] = (Half)rng.NextDouble();
+                writer.Seek(0xB0, SeekOrigin.Begin);
+                writer.Write((Half)rand[8]);
+
+                rand[9] = (float)rng.NextDouble();
+                writer.Seek(0x20, SeekOrigin.Begin);
+                writer.Write((float)rand[9]);
+
+                rand[10] = rng.NextDouble();
+                writer.Seek(0x50, SeekOrigin.Begin);
+                writer.Write((double)rand[10]);
+
+                rand[11] = Guid.NewGuid();
+                writer.Seek(0x60, SeekOrigin.Begin);
+                writer.Write((Guid)rand[11]);
+
+                stream.Position = 0;
+                var obj = reader.ReadObject<BasicStruct02>();
+
+                //the highest offset should always be read last
+                //so if no size is specified the position should end
+                //up at the highest offset + the size of the property
+                Assert.AreEqual(0xB2, stream.Position);
+                Assert.AreEqual(rand[0], obj.Property1);
+                Assert.AreEqual(rand[1], obj.Property2);
+                Assert.AreEqual(rand[2], obj.Property3);
+                Assert.AreEqual(rand[3], obj.Property4);
+                Assert.AreEqual(rand[4], obj.Property5);
+                Assert.AreEqual(rand[5], obj.Property6);
+                Assert.AreEqual(rand[6], obj.Property7);
+                Assert.AreEqual(rand[7], obj.Property8);
+                Assert.AreEqual(rand[8], obj.Property9);
+                Assert.AreEqual(rand[9], obj.Property10);
+                Assert.AreEqual(rand[10], obj.Property11);
+                Assert.AreEqual(rand[11], obj.Property12);
+            }
+        }
+    }
+}
---
+++ Reclaimer.Core.Tests/IO.Tests/Structures/Struct.cs
@@ -0,0 +1,81 @@
+﻿namespace Reclaimer.IO.Tests.Structures
+{
+    [FixedSize(0xFF)]
+    public struct BasicStruct01
+    {
+        [Offset(0x00)]
+        public sbyte Property1 { get; set; }
+
+        [Offset(0x10)]
+        public short Property2 { get; set; }
+
+        [Offset(0x20)]
+        public int Property3 { get; set; }
+
+        [Offset(0x30)]
+        public long Property4 { get; set; }
+
+        [Offset(0x40)]
+        public byte Property5 { get; set; }
+
+        [Offset(0x50)]
+        public ushort Property6 { get; set; }
+
+        [Offset(0x60)]
+        public uint Property7 { get; set; }
+
+        [Offset(0x70)]
+        public ulong Property8 { get; set; }
+
+        [Offset(0x80)]
+        public Half Property9 { get; set; }
+
+        [Offset(0x90)]
+        public float Property10 { get; set; }
+
+        [Offset(0xA0)]
+        public double Property11 { get; set; }
+
+        [Offset(0xB0)]
+        public Guid Property12 { get; set; }
+    }
+
+    public struct BasicStruct02
+    {
+        [Offset(0x70)]
+        public sbyte Property1 { get; set; }
+
+        [Offset(0x40)]
+        public short Property2 { get; set; }
+
+        [Offset(0x30)]
+        public int Property3 { get; set; }
+
+        [Offset(0x10)]
+        public long Property4 { get; set; }
+
+        [Offset(0x90)]
+        public byte Property5 { get; set; }
+
+        [Offset(0xA0)]
+        public ushort Property6 { get; set; }
+
+        [Offset(0x00)]
+        public uint Property7 { get; set; }
+
+        [Offset(0x80)]
+        public ulong Property8 { get; set; }
+
+        [Offset(0xB0)]
+        public Half Property9 { get; set; }
+
+        [Offset(0x20)]
+        public float Property10 { get; set; }
+
+        [Offset(0x50)]
+        public double Property11 { get; set; }
+
+        [Offset(0x60)]
+        public Guid Property12 { get; set; }
+    }
+}
