fe66101eb4e63d62016d7c34997e33656a045d93
Gravemind2401 <inferi_sententia@hotmail.com>
2023-09-03 11:19:24 +1000

    add tests to validate stream position when using derived reader class

---
+++ Reclaimer.Core.Tests/IO.Tests/DynamicRead/Derived.cs
@@ -0,0 +1,87 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Reclaimer.IO.Tests.DynamicRead
+{
+    public partial class DynamicRead
+    {
+        [DataTestMethod]
+        [DataRow(ByteOrder.LittleEndian)]
+        [DataRow(ByteOrder.BigEndian)]
+        public void Derived01(ByteOrder order)
+        {
+            var rng = new Random();
+            var rand = new int[2];
+            rng.NextBytes(MemoryMarshal.AsBytes<int>(rand));
+
+            using (var stream = new MemoryStream(new byte[500]))
+            using (var reader = new DerivedReader(stream, order))
+            using (var writer = new EndianWriter(stream, order))
+            {
+                writer.Seek(0x00, SeekOrigin.Begin);
+                foreach (var i in rand)
+                    writer.Write(i);
+
+                stream.Position = 0;
+                var obj = reader.ReadObject<DerivedTestClass01>();
+
+                //if no properties with [Offset] and no [FixedSize] then the position must remain where the derived class left it
+                //ie we must assume the derived class handled the reading and left the position at the end of the object
+                Assert.AreEqual(sizeof(int), stream.Position);
+                Assert.AreEqual(rand[0], obj.Property0);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(ByteOrder.LittleEndian)]
+        [DataRow(ByteOrder.BigEndian)]
+        public void Derived02(ByteOrder order)
+        {
+            var rng = new Random();
+            var rand = new int[2];
+            rng.NextBytes(MemoryMarshal.AsBytes<int>(rand));
+
+            using (var stream = new MemoryStream(new byte[500]))
+            using (var reader = new DerivedReader(stream, order))
+            using (var writer = new EndianWriter(stream, order))
+            {
+                writer.Seek(0x00, SeekOrigin.Begin);
+                foreach (var i in rand)
+                    writer.Write(i);
+
+                stream.Position = 0;
+                var obj = reader.ReadObject<DerivedTestClass02>();
+
+                //derived class read an int but there was another property, so position must remain at the end of the highest offset + sizeof property
+                Assert.AreEqual(sizeof(int) * 2, stream.Position);
+                Assert.AreEqual(rand[0], obj.Property0);
+                Assert.AreEqual(rand[1], obj.Property1);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(ByteOrder.LittleEndian)]
+        [DataRow(ByteOrder.BigEndian)]
+        public void Derived03(ByteOrder order)
+        {
+            var rng = new Random();
+            var rand = new int[2];
+            rng.NextBytes(MemoryMarshal.AsBytes<int>(rand));
+
+            using (var stream = new MemoryStream(new byte[500]))
+            using (var reader = new DerivedReader(stream, order))
+            using (var writer = new EndianWriter(stream, order))
+            {
+                writer.Seek(0x00, SeekOrigin.Begin);
+                foreach (var i in rand)
+                    writer.Write(i);
+
+                stream.Position = 0;
+                var obj = reader.ReadObject<DerivedTestClass03>();
+
+                //position must end up at the size defined by [FixedSize]
+                Assert.AreEqual(0x10, stream.Position);
+                Assert.AreEqual(rand[0], obj.Property0);
+            }
+        }
+    }
+}
---
+++ Reclaimer.Core.Tests/IO.Tests/Structures/Derived.cs
@@ -0,0 +1,57 @@
+﻿namespace Reclaimer.IO.Tests.Structures
+{
+    public class DerivedReader : EndianReader
+    {
+        public DerivedReader(Stream input, ByteOrder byteOrder)
+            : base(input, byteOrder)
+        { }
+
+        protected override object CreateInstance(Type type, double? version)
+        {
+            if (type == typeof(DerivedTestClass01))
+                return new DerivedTestClass01(this);
+
+            if (type == typeof(DerivedTestClass02))
+                return new DerivedTestClass02(this);
+
+            if (type == typeof(DerivedTestClass03))
+                return new DerivedTestClass03(this);
+
+            return base.CreateInstance(type, version);
+        }
+    }
+
+    public class DerivedTestClass01
+    {
+        public int Property0 { get; }
+
+        public DerivedTestClass01(EndianReader reader)
+        {
+            Property0 = reader.ReadInt32();
+        }
+    }
+
+    public class DerivedTestClass02
+    {
+        public int Property0 { get; }
+
+        [Offset(0x04)]
+        public int Property1 { get; set; }
+
+        public DerivedTestClass02(EndianReader reader)
+        {
+            Property0 = reader.ReadInt32();
+        }
+    }
+
+    [FixedSize(0x10)]
+    public class DerivedTestClass03
+    {
+        public int Property0 { get; }
+
+        public DerivedTestClass03(EndianReader reader)
+        {
+            Property0 = reader.ReadInt32();
+        }
+    }
+}
