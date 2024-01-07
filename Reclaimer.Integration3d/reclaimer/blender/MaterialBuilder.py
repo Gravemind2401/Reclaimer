2f252a7ef5d12be7d972d70f8eeb01ad06561bcf
Gravemind2401 <inferi_sententia@hotmail.com>
2024-01-07 12:50:25 +1100

    basic shader node positioning for blender shaders

--- Reclaimer.Integration3d/reclaimer/blender/MaterialBuilder.py
+++ Reclaimer.Integration3d/reclaimer/blender/MaterialBuilder.py
@@ -1,5 +1,5 @@
 import bpy
-from typing import Dict
+from typing import Dict, List, Tuple
 
 from ..src.SceneReader import *
 from ..src.ImportOptions import *
@@ -11,6 +11,8 @@ __all__ = [
     'MaterialBuilder'
 ]
 
+TextureFrame = Tuple[bpy.types.NodeFrame, bpy.types.ShaderNodeTexImage]
+
 def _create_uvscale_node(material: bpy.types.Material, input: TextureMapping, image_node: bpy.types.Node) -> bpy.types.Node:
     if input.tiling == (1, 1):
         return None
@@ -20,6 +22,14 @@ def _create_uvscale_node(material: bpy.types.Material, input: TextureMapping, im
     scale_node.inputs['Y'].default_value = input.tiling[1]
     material.node_tree.links.new(image_node.inputs['Vector'], scale_node.outputs['Vector'])
     return scale_node
+
+def _create_texture_frame(material: bpy.types.Material, image_node: bpy.types.ShaderNode, scale_node: bpy.types.ShaderNode) -> bpy.types.NodeFrame:
+    frame = material.node_tree.nodes.new('NodeFrame')
+    image_node.parent = frame
+    if scale_node:
+        scale_node.parent = frame
+        scale_node.location = (-200, -100)
+    return frame
 
 
 class MaterialBuilder:
@@ -48,7 +58,7 @@ class MaterialBuilder:
             TEXTURE_USAGE.NORMAL: self._create_bump
         }
 
-        usage_lookup = {
+        usage_lookup: Dict[str, List[TextureFrame]] = {
             TEXTURE_USAGE.BLEND: [],
             TEXTURE_USAGE.DIFFUSE: [],
             TEXTURE_USAGE.NORMAL: []
@@ -58,75 +68,103 @@ class MaterialBuilder:
             if not input.texture_usage in factory_lookup:
                 continue
 
-            # TODO: node tree layout
             usage_lookup[input.texture_usage].append(factory_lookup[input.texture_usage](result, input))
 
         diffuse_images = usage_lookup[TEXTURE_USAGE.DIFFUSE]
         bump_images = usage_lookup[TEXTURE_USAGE.NORMAL]
 
         if usage_lookup[TEXTURE_USAGE.BLEND]:
-            blend_image = usage_lookup[TEXTURE_USAGE.BLEND][0]
+            x_position = -1400
+            y_position = 1400
+
+            def next_position() -> Tuple[float, float]:
+                nonlocal x_position, y_position
+                next = (x_position, y_position)
+                y_position = y_position - 400
+                return next
+
+            blend_frame, blend_image = usage_lookup[TEXTURE_USAGE.BLEND][0]
+            blend_frame.location = (-1000, 100)
 
             # TODO: assign blend inputs based on specific blend channel rather than index
+            # TODO: specular for blend
+            # TODO: specular for standard mats
 
             if diffuse_images:
                 diffuse_blend = create_group_node(result, 'Blend Mask')
+                diffuse_blend.location = (-400, 600)
                 result.node_tree.links.new(diffuse_blend.inputs['Mask RGB'], blend_image.outputs['Color'])
 
-                for diffuse_image, blend_input in zip(diffuse_images, ['R', 'G', 'B', 'A']):
+                for (frame, diffuse_image), blend_input in zip(diffuse_images, ['R', 'G', 'B', 'A']):
+                    frame.location = next_position()
                     result.node_tree.links.new(diffuse_blend.inputs[blend_input], diffuse_image.outputs['Color'])
 
                 result.node_tree.links.new(bsdf.inputs['Base Color'], diffuse_blend.outputs['Color'])
 
-
             if bump_images:
                 bump_blend = create_group_node(result, 'Blend Mask')
+                bump_blend.location = (-400, -400)
                 result.node_tree.links.new(bump_blend.inputs['Mask RGB'], blend_image.outputs['Color'])
 
-                for bump_image, blend_input in zip(bump_images, ['R', 'G', 'B', 'A']):
+                for (frame, bump_image), blend_input in zip(bump_images, ['R', 'G', 'B', 'A']):
+                    frame.location = next_position()
                     result.node_tree.links.new(bump_blend.inputs[blend_input], bump_image.outputs['Color'])
 
                 normal_node = create_group_node(result, 'DX Normal Map')
+                normal_node.location = (-200, -400)
                 result.node_tree.links.new(normal_node.inputs['Color'], bump_blend.outputs['Color'])
                 result.node_tree.links.new(bsdf.inputs['Normal'], normal_node.outputs['Normal'])
         else:
+            # should only ever be max 1 of each input type
             if diffuse_images:
-                diffuse_image = diffuse_images[0]
+                frame, diffuse_image = diffuse_images[0]
+                frame.location = (-400, 300)
                 result.node_tree.links.new(bsdf.inputs['Base Color'], diffuse_image.outputs['Color'])
             if bump_images:
-                bump_image = bump_images[0]
+                frame, bump_image = bump_images[0]
+                frame.location = (-500, -100)
                 normal_node = create_group_node(result, 'DX Normal Map')
+                normal_node.location = (-200, -150)
                 result.node_tree.links.new(normal_node.inputs['Color'], bump_image.outputs['Color'])
                 result.node_tree.links.new(bsdf.inputs['Normal'], normal_node.outputs['Normal'])
 
         return result
 
-    def _create_blend(self, material: bpy.types.Material, input: TextureMapping) -> bpy.types.Node:
+    def _create_blend(self, material: bpy.types.Material, input: TextureMapping) -> TextureFrame:
         image_node = self._create_texture(material, input)
         image_node.image.alpha_mode = 'CHANNEL_PACKED'
 
         scale_node = _create_uvscale_node(material, input, image_node)
 
-        return image_node
+        frame = _create_texture_frame(material, image_node, scale_node)
+        frame.label = input.texture_usage
 
-    def _create_diffuse(self, material: bpy.types.Material, input: TextureMapping) -> bpy.types.Node:
+        return (frame, image_node)
+
+    def _create_diffuse(self, material: bpy.types.Material, input: TextureMapping) -> TextureFrame:
         image_node = self._create_texture(material, input)
         image_node.image.alpha_mode = 'CHANNEL_PACKED'
 
         scale_node = _create_uvscale_node(material, input, image_node)
 
-        return image_node
+        frame = _create_texture_frame(material, image_node, scale_node)
+        frame.label = input.texture_usage
 
-    def _create_bump(self, material: bpy.types.Material, input: TextureMapping) -> bpy.types.Node:
+        return (frame, image_node)
+
+    def _create_bump(self, material: bpy.types.Material, input: TextureMapping) -> TextureFrame:
         image_node = self._create_texture(material, input)
         image_node.image.alpha_mode = 'NONE'
         image_node.image.colorspace_settings.name = 'Non-Color'
 
         scale_node = _create_uvscale_node(material, input, image_node)
 
-        return image_node
+        frame = _create_texture_frame(material, image_node, scale_node)
+        frame.label = input.texture_usage
 
-    def _create_texture(self, material: bpy.types.Material, input: TextureMapping) -> bpy.types.Node:
+        return (frame, image_node)
+
+    def _create_texture(self, material: bpy.types.Material, input: TextureMapping) -> bpy.types.ShaderNodeTexImage:
         scene, OPTIONS = self._scene, self._options
 
         # ensure each image is only loaded once, regardless of how many materials it gets used in
