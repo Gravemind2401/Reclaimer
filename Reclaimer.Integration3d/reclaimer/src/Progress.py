03b0ad5a0f80fe949e576eebe228609e9bea6ff5
Gravemind2401 <inferi_sententia@hotmail.com>
2024-02-19 18:00:07 +1100

    progress percentage in rmf import

--- Reclaimer.Integration3d/reclaimer/src/Progress.py
+++ Reclaimer.Integration3d/reclaimer/src/Progress.py
@@ -5,13 +5,25 @@ from ..src.ImportOptions import ImportOptions
 class ProgressCallback:
     cancel_requested: bool = False
 
-    object_count: int = 0
     material_count: int = 0
     mesh_count: int = 0
+    object_count: int = 0
 
-    object_progress: int = 0
     material_progress: int = 0
     mesh_progress: int = 0
+    object_progress: int = 0
+
+    @property
+    def material_percent(self) -> float:
+        return int(self.material_progress / self.material_count * 100) / 100
+
+    @property
+    def mesh_percent(self) -> float:
+        return int(self.mesh_progress / self.mesh_count * 100) / 100
+
+    @property
+    def object_percent(self) -> float:
+        return int(self.object_progress / self.object_count * 100) / 100
 
     def __init__(self, filter: SceneFilter, options: ImportOptions):
         self.object_count = filter.count_objects()
@@ -20,16 +32,16 @@ class ProgressCallback:
         if options.IMPORT_MESHES:
             self.mesh_count = filter.count_meshes()
 
-    def increment_objects(self):
-        self.object_progress = self.object_progress + 1
-        self._refresh()
-
     def increment_materials(self):
         self.material_progress = self.material_progress + 1
         self._refresh()
 
     def increment_meshes(self):
         self.mesh_progress = self.mesh_progress + 1
+        self._refresh()
+
+    def increment_objects(self):
+        self.object_progress = self.object_progress + 1
         self._refresh()
 
     def complete(self):
--- Reclaimer.Integration3d/reclaimer/ui/ProgressDialog.py
+++ Reclaimer.Integration3d/reclaimer/ui/ProgressDialog.py
@@ -62,9 +62,9 @@ class ProgressDialog(QtWidgets.QDialog, ProgressCallback):
         self._widget.progressBar_meshes.setValue(self.mesh_progress)
         self._widget.progressBar_objects.setValue(self.object_progress)
 
-        self._widget.label_materials_progress.setText(f'{self.material_progress} / {self.material_count}')
-        self._widget.label_meshes_progress.setText(f'{self.mesh_progress} / {self.mesh_count}')
-        self._widget.label_objects_progress.setText(f'{self.object_progress} / {self.object_count}')
+        self._widget.label_materials_progress.setText(f'{self.material_progress} / {self.material_count} ({self.material_percent:.0%})')
+        self._widget.label_meshes_progress.setText(f'{self.mesh_progress} / {self.mesh_count} ({self.mesh_percent:.0%})')
+        self._widget.label_objects_progress.setText(f'{self.object_progress} / {self.object_count} ({self.object_percent:.0%})')
 
         QtWidgets.QApplication.processEvents()
 
