2d357e50519dce2881f67a9ae6c882bd77bef155
Gravemind2401 <inferi_sententia@hotmail.com>
2024-06-08 16:22:19 +1000

    fix bitmap viewer channel toggles not working for a8r8g8b8 bitmaps

--- Reclaimer.Core/Drawing/DdsConvert.cs
+++ Reclaimer.Core/Drawing/DdsConvert.cs
@@ -156,7 +156,13 @@ namespace Reclaimer.Drawing
             var pixels = DecompressPixelData(args.Bgr24);
 
             if (args.UseChannelMask)
+            {
+                //MaskChannels() will alter the array that gets passed in, so make sure it doesnt modify the original dds pixels array
+                if (ReferenceEquals(pixels, data))
+                    pixels = (byte[])data.Clone();
+
                 MaskChannels(pixels, args.Options);
+            }
 
             var source = BitmapSource.Create(Width, virtualHeight, dpi, dpi, args.Format, null, pixels, Width * args.Bpp);
 
