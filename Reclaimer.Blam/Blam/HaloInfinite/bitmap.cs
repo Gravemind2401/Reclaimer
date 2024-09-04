6b74460cee85693244cd60437dab90241409f136
Surasia <74399067+Surasia@users.noreply.github.com>
2024-09-04 20:08:03 +0200

    blam: add support for bitmap data inside tag

--- Reclaimer.Blam/Blam/HaloInfinite/bitmap.cs
+++ Reclaimer.Blam/Blam/HaloInfinite/bitmap.cs
@@ -24,47 +24,50 @@ namespace Reclaimer.Blam.HaloInfinite
 
         CubemapLayout IBitmap.CubeLayout => CubemapLayout.NonCubemap;
 
+        #endregion
+
         public DdsImage ToDds(int index)
         {
-            var isChunk = false;
-
-            if (index >= Item.ResourceCount)
-                System.Diagnostics.Debugger.Break();
-
             var resourceIndex = Item.ResourceIndex + Item.ResourceCount - 1;
             var resource = Item.Module.Items[Item.Module.Resources[resourceIndex]];
             var submap = Bitmaps[index];
 
             // Skip to the mip after the empty one from HD1.
-            // Will implement HD1 soon.
             if (resource.DataOffsetFlags.HasFlag(DataOffsetFlags.UseHD1))
             {
                 resourceIndex -= 1;
                 resource = Item.Module.Items[Item.Module.Resources[resourceIndex]];
-                submap.Width = (short)(submap.Width / 2);
-                submap.Height = (short)(submap.Height / 2);
-            }
-            
-
-            byte[] data;
-            using (var reader = resource.CreateReader())
-            {
-                if (!isChunk)
-                {
-                    if (resource.Flags.HasFlag(FileEntryFlags.HasBlocks))
-                        reader.Seek(resource.UncompressedHeaderSize + resource.UncompressedTagSize, SeekOrigin.Begin);
-                }
-
-                data = reader.ReadBytes((int)resource.TotalUncompressedSize);
+                submap.Width /= 2;
+                submap.Height /= 2;
             }
 
-            //todo: cubemap check
+            byte[] data = ReadResourceData(index, resource);
+
             var format = TextureUtils.DXNSwap(submap.BitmapFormat, true);
             var props = new BitmapProperties(submap.Width, submap.Height, format, "Texture2D");
             return TextureUtils.GetDds(props, data, false);
         }
 
-        #endregion
+        private byte[] ReadResourceData(int index, ModuleItem resource)
+        {
+            using (var reader = index < Item.ResourceCount
+                                    ? resource.CreateReader()
+                                    : Item.CreateReader())
+            {
+                if (resource.Flags.HasFlag(FileEntryFlags.HasBlocks))
+                {
+                    reader.Seek(resource.UncompressedHeaderSize + resource.UncompressedTagSize, SeekOrigin.Begin);
+                }
+                else if (index >= Item.ResourceCount)
+                {
+                    reader.Seek(Item.UncompressedHeaderSize + Item.UncompressedTagSize, SeekOrigin.Begin);
+                    // Early return if bitmap data is contained inside tag.
+                    return reader.ReadBytes((int)Item.UncompressedActualResourceSize);
+                }
+                
+                return reader.ReadBytes((int)resource.TotalUncompressedSize);
+            }
+        }
     }
 
     [FixedSize(40)]
