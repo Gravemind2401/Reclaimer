29874b7da66566682e47176eced2485bbf8f9f81
Surasia <74399067+Surasia@users.noreply.github.com>
2024-09-18 07:38:48 +0200

    blam(infinite): remove unnecessary calculations
    im dumb, c# doesnt mutate arguments passed to it directly

--- Reclaimer.Blam/Blam/HaloInfinite/scenario_structure_bsp.cs
+++ Reclaimer.Blam/Blam/HaloInfinite/scenario_structure_bsp.cs
@@ -45,28 +45,16 @@ namespace Reclaimer.Blam.HaloInfinite
                     var model = tag.GetModelContent();
 
                     var sceneItem = new ObjectPlacement(model);
-                    var transformMatrix = instance.Transform;
-
-                    var forward = new Vector3(transformMatrix.M31, transformMatrix.M32, transformMatrix.M33);
-                    var left = new Vector3(transformMatrix.M11, transformMatrix.M12, transformMatrix.M13);
-                    var up = new Vector3(transformMatrix.M21, transformMatrix.M22, transformMatrix.M23);
 
                     var rotMat = new Matrix4x4(
-                        left.X, left.Y, left.Z, 0,
-                        up.X, up.Y, up.Z, 0,
-                        forward.X, forward.Y, forward.Z, 0,
+                        instance.Transform.M11, instance.Transform.M12, instance.Transform.M13, 0,
+                        instance.Transform.M21, instance.Transform.M22, instance.Transform.M23, 0,
+                        instance.Transform.M31, instance.Transform.M32, instance.Transform.M33, 0,
                         0, 0, 0, 1
                     );
 
                     var rotation = Quaternion.CreateFromRotationMatrix(rotMat);
-
-                    var eulerAngles = QuaternionToEuler(rotation);
-                    eulerAngles = Vector3.Transform(eulerAngles, Quaternion.CreateFromAxisAngle(Vector3.UnitX, MathF.PI / 180 * -90));
-
-                    var position = instance.Transform.Translation;
-                    var rotatedPos = Vector4.Transform(new Vector4(position, 1.0f), rotMat);
-
-                    sceneItem.SetTransform((Vector3)(instance.Scale), position, rotation);
+                    sceneItem.SetTransform((Vector3)instance.Scale, instance.Transform.Translation, rotation);
                     bspGroup.ChildObjects.Add(sceneItem);
                 }
                 catch
@@ -80,18 +68,6 @@ namespace Reclaimer.Blam.HaloInfinite
 
             return scene;
         }
-
-        private static Vector3 QuaternionToEuler(Quaternion q)
-        {
-            float roll = MathF.Atan2(2.0f * (q.Y * q.Z + q.W * q.X), q.W * q.W - q.X * q.X - q.Y * q.Y + q.Z * q.Z);
-            float pitch = MathF.Asin(-2.0f * (q.X * q.Z - q.W * q.Y));
-            float yaw = MathF.Atan2(2.0f * (q.X * q.Y + q.W * q.Z), q.W * q.W + q.X * q.X - q.Y * q.Y - q.Z * q.Z);
-
-            return new Vector3(roll, pitch, yaw);
-        }
-
-
-
         #endregion
     }
 
